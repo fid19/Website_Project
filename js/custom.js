b89227944a35f304a8c2582a0dccb6507e688708
fid19 <melblenk@gmail.com>
2020-09-03 02:32:07 +0100

    website project original

---
+++ js/custom.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+ 
+  var owl = $("#owl-demo");
+ 
+  owl.owlCarousel({
+     
+      itemsCustom : [
+        [0, 2],
+        [450, 3],
+        [600, 3],
+        [700, 3],
+        [1000, 4],
+        [1200, 4],
+        [1400, 4],
+        [1600, 4]
+      ],
+      navigation : true
+ 
+  });
+ 
+});
+
